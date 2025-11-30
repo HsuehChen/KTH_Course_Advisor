d233bd93b239fe5004365a872a23cbd1241e30d7
FearsomeRover <bujgergo@gmail.com>
2025-11-30 11:54:12 +0100

    init repo

---
+++ src/main/kotlin/furhatos/app/courseadvisor/main.kt
@@ -0,0 +1,15 @@
+package furhatos.app.courseadvisor
+
+import furhatos.app.courseadvisor.flow.Init
+import furhatos.flow.kotlin.Flow
+import furhatos.skills.Skill
+
+class CourseadvisorSkill : Skill() {
+    override fun start() {
+        Flow().run(Init)
+    }
+}
+
+fun main(args: Array<String>) {
+    Skill.main(args)
+}
